24b4af3dd926707af86735f4939eacfb88d0d3ec
Joey  Bull <joeybull@10-116-30-48.wireless.oregonstate.edu>
2023-11-12 20:04:53 -0800

    firefighting is cool

---
+++ piglatin.py
@@ -0,0 +1,16 @@
+# Get input from user
+input_sentence = input("Give me a sentence:")
+# break up the sentence into words
+split_sentence = input_sentence.split("")
+# new sentence empty string
+print(input_sentence)
+#iterate through each word
+new_sentence =""
+# if the word starts with a vowel add "way"
+
+for word in split_sentence:
+# else find the first vowel and move verything before to th end with "ay"
+if word [0]
+
+# add the pig latin word to the new sentence + space
+
